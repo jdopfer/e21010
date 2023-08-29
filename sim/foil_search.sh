a0b4c5f444583bbca558ad2a9e6e4105b33e5396
Erik Jensen <ej@phys.au.dk>
2023-08-29 13:21:36 +0200

    add 2-proton config files, update foil search script to most recent iteration

--- sim/foil_search.sh
+++ sim/foil_search.sh
@@ -5,11 +5,13 @@ mkdir -p data/foil_search
 
 source /usr/local/bin/geant4.sh
 
-X=$1
-Y=$2
+X=`echo $(bc <<< "$1*1")`
+Y=`echo $(bc <<< "$2*1")`
+Z=`echo $(bc <<< "$3*1")`
 
 XX=`echo $(bc <<< "${X}*10") | cut -d. -f1`
 YY=`echo $(bc <<< "${Y}*10") | cut -d. -f1`
+ZZ=`echo $(bc <<< "${Z}*10") | cut -d. -f1`
 
 if [ $XX -lt 0 ] ; then
   XX="neg$(bc <<< "${XX}*-1")"
@@ -17,9 +19,21 @@ fi
 if [ $YY -lt 0 ] ; then
   YY="neg$(bc <<< "${YY}*-1")"
 fi
+if [ $ZZ -lt 0 ] ; then
+  ZZ="neg$(bc <<< "${ZZ}*-1")"
+fi
 
-sed -e "s/\/G4Sim\/outputFile .*/\/G4Sim\/outputFile ..\/data\/foil_search\/${XX}_${YY}.root/" \
-    -e "s/\/G4Sim\/isotropic\/position .*/\/G4Sim\/isotropic\/position ${X} ${Y} 0. mm/" \
-    res/macros/iso_proton.mac > res/macros/tmp/${XX}_${YY}.mac
+gsed -e "s/\/G4Sim\/outputFile .*/\/G4Sim\/outputFile ..\/data\/foil_search\/${XX}_${YY}_${ZZ}.root/" \
+          -e "s/\/G4Sim\/target\/position .*/\/G4Sim\/target\/position 0. 0. ${Z} mm/" \
+          -e "s/\/G4Sim\/isotropic\/position .*/\/G4Sim\/isotropic\/position ${X} ${Y} 0. mm/" \
+          res/macros/iso_proton.mac > res/macros/tmp/${XX}_${YY}_${ZZ}.mac
 
-../cmake-build-debug/sim/sim res/macros/tmp/${XX}_${YY}.mac
+../build/sim/sim res/macros/tmp/${XX}_${YY}_${ZZ}.mac
+
+#echo "${X} ${Y} ${Z} ${XX} ${YY} ${ZZ}"
+
+#printf "(\"${XX}\", \"${YY}\"), " >> combinations_xy.dat
+#printf "(${X}, ${Y}), " >> combinations_num_xy.dat
+
+#printf "(\"${YY}\", \"${ZZ}\"), " >> combinations_yz.dat
+#printf "(${Y}, ${Z}), " >> combinations_num_yz.dat
